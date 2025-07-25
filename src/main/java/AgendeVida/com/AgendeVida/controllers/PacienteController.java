46ccf77160054adc31045089145da4154a3f1933
Andre Luiz <andre2kluiz@gmail.com>
2025-07-24 21:01:56 -0300

    feat: crud para o pacienteController

--- src/main/java/AgendeVida/com/AgendeVida/controllers/PacienteController.java
+++ src/main/java/AgendeVida/com/AgendeVida/controllers/PacienteController.java
@@ -1,7 +1,13 @@
 package AgendeVida.com.AgendeVida.controllers;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +27,26 @@ public class PacienteController {
     @PostMapping
     public ResponseEntity<PacienteResponseDTO> cadastroPaciente(@RequestBody PacienteRequestDTO dto) {
         return ResponseEntity.ok(pacienteService.cadastroPaciente(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> buscarPorId(@PathVariable String id) {
+        return ResponseEntity.ok(pacienteService.buscarPorId(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteResponseDTO>> listarTodos() {
+        return ResponseEntity.ok(pacienteService.listarTodos());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> atualizar(@PathVariable String id, @RequestBody PacienteRequestDTO dto) {
+        return ResponseEntity.ok(pacienteService.atualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable String id) {
+        pacienteService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
